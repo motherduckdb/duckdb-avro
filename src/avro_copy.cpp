095d1b8fe5e469b52b36042a3e397b1c743f3079
Tishj <t_b@live.nl>
2025-05-29 11:58:19 +0200

    can correctly roundtrip the duckdb names+types to avro json schema

--- src/avro_copy.cpp
+++ src/avro_copy.cpp
@@ -3,79 +3,179 @@
 #include "duckdb/common/enums/file_compression_type.hpp"
 #include "duckdb/common/file_system.hpp"
 #include "duckdb/function/function.hpp"
+#include "yyjson.hpp"
+#include "duckdb/common/printer.hpp"
+
+using namespace duckdb_yyjson; // NOLINT
 
 namespace duckdb {
 
-struct WriteAvroBindData : public FunctionData {
-public:
-	WriteAvroBindData() {}
-public:
-	unique_ptr<FunctionData> Copy() const override {
-		//! FIXME: actually implement
-		return make_uniq<WriteAvroBindData>();
+static string ConvertTypeToAvro(const LogicalType &type) {
+	switch (type.id()) {
+	case LogicalTypeId::VARCHAR:
+		return "string";
+	case LogicalTypeId::BLOB:
+		return "bytes";
+	case LogicalTypeId::INTEGER:
+		return "int";
+	case LogicalTypeId::BIGINT:
+		return "long";
+	case LogicalTypeId::FLOAT:
+		return "float";
+	case LogicalTypeId::DOUBLE:
+		return "double";
+	case LogicalTypeId::BOOLEAN:
+		return "boolean";
+	case LogicalTypeId::SQLNULL:
+		return "null";
+	case LogicalTypeId::STRUCT:
+		return "record";
+	case LogicalTypeId::ENUM:
+		return "enum";
+	case LogicalTypeId::LIST:
+		return "array";
+	case LogicalTypeId::MAP:
+		//! This uses a 'logicalType': map, and a struct as 'items'
+		return "array";
+	default:
+		throw NotImplementedException("Can't convert logical type '%s' to Avro type", type.ToString());
+	};
+
+	//! FIXME: we don't have support for 'FIXED' currently (a fixed size blob)
+}
+
+static yyjson_mut_val *CreateJSONType(yyjson_mut_doc *doc, const string &name, const LogicalType &type, optional_ptr<const LogicalType> parent_type) {
+	auto object = yyjson_mut_obj(doc);
+
+	if (!name.empty()) {
+		yyjson_mut_obj_add_str(doc, object, "name", name.c_str());
 	}
 
-	bool Equals(const FunctionData &other) const override {
-		//! FIXME: actually implement
-		return true;
+	//! TODO: actually get the field ids
+	int32_t field_id = 42;
+
+	switch (type.id()) {
+		case LogicalTypeId::STRUCT: {
+			auto &struct_children = StructType::GetChildTypes(type);
+			auto fields = yyjson_mut_obj_add_arr(doc, object, "fields");
+			for (auto &it : struct_children) {
+				auto &child_name = it.first;
+				auto &child_type = it.second;
+				yyjson_mut_arr_add_val(fields, CreateJSONType(doc, child_name, child_type, &type));
+			}
+			break;
+		}
+		case LogicalTypeId::LIST: {
+			//! NOTE: This is seemingly a work-around of a limitation in either the Avro spec, or the avro-c implementation
+			//! 'array' can not be directly part of a struct field
+			auto union_type = yyjson_mut_obj_add_arr(doc, object, "type");
+			//! First item of the union is null, to indicate that the field is nullable
+			yyjson_mut_arr_add_str(doc, union_type, "null");
+
+			yyjson_mut_obj_add_int(doc, object, "element-id", field_id);
+
+			auto list_object = yyjson_mut_obj(doc);
+
+			yyjson_mut_obj_add_str(doc, list_object, "type", "array");
+			auto &list_child = ListType::GetChildType(type);
+			if (list_child.IsNested()) {
+				yyjson_mut_obj_add_val(doc, list_object, "items", CreateJSONType(doc, "element", list_child, &type));
+			} else {
+				yyjson_mut_obj_add_strcpy(doc, list_object, "items", ConvertTypeToAvro(list_child).c_str());
+			}
+
+			yyjson_mut_arr_add_val(union_type, list_object);
+			break;
+		}
+		case LogicalTypeId::MAP: {
+			auto &child_type = ListType::GetChildType(type);
+			yyjson_mut_obj_add_strcpy(doc, object, "logicalType", "map");
+			yyjson_mut_obj_add_val(doc, object, "items", CreateJSONType(doc, "key_value", child_type, &type));
+			break;
+		}
+		default:
+			break;
 	}
 
-public:
-	//! TODO: store any data immutable data created during bind
-	optional_ptr<GlobalFunctionData> global_state;
-};
-
-struct WriteAvroLocalState : public LocalFunctionData {
-public:
-	WriteAvroLocalState() {}
-public:
-	//! TODO: store any data required by a single thread to write to the Avro file
-};
-
-struct WriteAvroGlobalState : public GlobalFunctionData {
-public:
-	WriteAvroGlobalState(FileSystem &fs, const string &file_path)
-	    : fs(fs) {
-		handle = fs.OpenFile(file_path, FileFlags::FILE_FLAGS_WRITE | FileFlags::FILE_FLAGS_FILE_CREATE_NEW | FileLockType::WRITE_LOCK | FileCompressionType::AUTO_DETECT);
+	if (type.id() != LogicalTypeId::LIST && type.id() != LogicalTypeId::MAP) {
+		yyjson_mut_obj_add_strcpy(doc, object, "type", ConvertTypeToAvro(type).c_str());
+		yyjson_mut_obj_add_int(doc, object, "field-id", field_id);
 	}
 
-	//! Write a finished data block to the file
-	void WriteDataBlock(const_data_ptr_t data, idx_t size) {
-		lock_guard<mutex> flock(lock);
-		handle->Write((void *)data, size);
+	return object;
+}
+
+static string CreateJSONSchema(const vector<string> &names, const vector<LogicalType> &types) {
+	auto doc = yyjson_mut_doc_new(nullptr);
+	auto root_object = yyjson_mut_obj(doc);
+	yyjson_mut_doc_set_root(doc, root_object);
+	yyjson_mut_obj_add_str(doc, root_object, "type", "record");
+	//! TODO: get this name from the bind input
+	yyjson_mut_obj_add_str(doc, root_object, "name", "manifest_file");
+	auto array = yyjson_mut_obj_add_arr(doc, root_object, "fields");
+
+	//! Add all the fields
+	D_ASSERT(names.size() == types.size());
+	for (idx_t i = 0; i < names.size(); i++) {
+		auto &name = names[i];
+		auto &type = types[i];
+		yyjson_mut_arr_add_val(array, CreateJSONType(doc, name, type, nullptr));
 	}
 
-	idx_t FileSize() {
-		lock_guard<mutex> flock(lock);
-		return handle->GetFileSize();
+	//! Write the result to a string
+	auto data = yyjson_mut_val_write_opts(root_object, YYJSON_WRITE_ALLOW_INF_AND_NAN, nullptr, nullptr, nullptr);
+	if (!data) {
+		yyjson_mut_doc_free(doc);
+		throw InvalidInputException("Could not create a JSON representation of the table schema, yyjson failed");
+	}
+	auto res = string(data);
+	free(data);
+	yyjson_mut_doc_free(doc);
+	return res;
+}
+
+WriteAvroBindData::WriteAvroBindData(const vector<string> &names, const vector<LogicalType> &types) {
+	//! TODO: create the avro schema from the provided names and types
+	//! Actually, looking through the code of 'avro_schema_from_json_literal', it might be easier to just generate JSON and use that method
+	auto json_schema = CreateJSONSchema(names, types);
+	Printer::Print(json_schema);
+	if (avro_schema_from_json_length(json_schema.c_str(), json_schema.size(), &schema)) {
+		throw InvalidInputException(avro_strerror());
 	}
 
-	FileSystem &fs;
-	//! The mutex for writing to the physical file
-	mutex lock;
-	//! The file handle to write to
-	unique_ptr<FileHandle> handle;
+	auto t = 1 + 42;
+	(void)t;
+}
 
-	//! The writer for the file
-	avro_file_writer_t writer;
-	//! The avro schema created from the input
-	avro_schema_t avro_schema;
-};
+WriteAvroBindData::~WriteAvroBindData() {
+	//! Make sure to free the schema
+	avro_schema_decref(schema);
+}
+
+WriteAvroGlobalState::WriteAvroGlobalState(ClientContext &context, FunctionData &bind_data_p, FileSystem &fs, const string &file_path)
+	: stream(Allocator::Get(context), BUFFER_SIZE), fs(fs) {
+	handle = fs.OpenFile(file_path, FileFlags::FILE_FLAGS_WRITE | FileFlags::FILE_FLAGS_FILE_CREATE_NEW | FileLockType::WRITE_LOCK | FileCompressionType::AUTO_DETECT);
+	writer = avro_writer_memory(const_char_ptr_cast(stream.GetData()), stream.GetCapacity());
+	auto &bind_data = bind_data_p.Cast<WriteAvroBindData>();
+
+	if (avro_file_writer_create_from_writer(writer, bind_data.schema, &file_writer)) {
+		throw InvalidInputException(avro_strerror());
+	}
+}
 
 static unique_ptr<FunctionData> WriteAvroBind(ClientContext &context, CopyFunctionBindInput &input, const vector<string> &names, const vector<LogicalType> &sql_types) {
-	auto res = make_uniq<WriteAvroBindData>();
-
+	auto res = make_uniq<WriteAvroBindData>(names, sql_types);
 	return res;
 }
 
 static unique_ptr<LocalFunctionData> WriteAvroInitializeLocal(ExecutionContext &context, FunctionData &bind_data) {
+	//! Local state unused for now, since we only support single threaded Avro writes currently
 	auto res = make_uniq<WriteAvroLocalState>();
-
 	return res;
 }
 
 static unique_ptr<GlobalFunctionData> WriteAvroInitializeGlobal(ClientContext &context, FunctionData &bind_data_p, const string &file_path) {
-	auto res = make_uniq<WriteAvroGlobalState>(FileSystem::GetFileSystem(context), file_path);
+	auto res = make_uniq<WriteAvroGlobalState>(context, bind_data_p, FileSystem::GetFileSystem(context), file_path);
 
 	auto &bind_data = bind_data_p.Cast<WriteAvroBindData>();
 	bind_data.global_state = res.get();
@@ -106,6 +206,11 @@ static void WriteAvroFinalize(ClientContext &context, FunctionData &bind_data, G
 	return;
 }
 
+CopyFunctionExecutionMode WriteAvroExecutionMode(bool preserve_insertion_order, bool supports_batch_index) {
+	//! For now we only support single-threaded writes to Avro
+	return CopyFunctionExecutionMode::REGULAR_COPY_TO_FILE;
+}
+
 CopyFunction AvroCopyFunction::Create() {
 	CopyFunction function("avro");
 	function.extension = "avro";
@@ -116,6 +221,7 @@ CopyFunction AvroCopyFunction::Create() {
 	function.copy_to_sink = WriteAvroSink;
 	function.copy_to_combine = WriteAvroCombine;
 	function.copy_to_finalize = WriteAvroFinalize;
+	function.execution_mode = WriteAvroExecutionMode;
 	return function;
 }
 
--- src/include/avro_copy.hpp
+++ src/include/avro_copy.hpp
@@ -2,11 +2,76 @@
 
 #include "duckdb/function/copy_function.hpp"
 #include <avro.h>
+#include "duckdb/common/serializer/memory_stream.hpp"
+#include "duckdb/common/file_system.hpp"
 
 namespace duckdb {
 
 struct AvroCopyFunction {
 	static CopyFunction Create();
+};
+
+struct WriteAvroBindData : public FunctionData {
+public:
+	WriteAvroBindData(const vector<string> &names, const vector<LogicalType> &types);
+	WriteAvroBindData() {}
+	virtual ~WriteAvroBindData();
+public:
+	unique_ptr<FunctionData> Copy() const override {
+		//! FIXME: actually implement
+		return make_uniq<WriteAvroBindData>();
+	}
+
+	bool Equals(const FunctionData &other) const override {
+		//! FIXME: actually implement
+		return true;
+	}
+
+public:
+	//! TODO: store any data immutable data created during bind
+	optional_ptr<GlobalFunctionData> global_state;
+	//! The schema of the file to write
+	avro_schema_t schema;
+};
+
+struct WriteAvroLocalState : public LocalFunctionData {
+public:
+	WriteAvroLocalState() {}
+public:
+	//! The interface through which new avro values are created
+	avro_value_iface_t interface;
+};
+
+struct WriteAvroGlobalState : public GlobalFunctionData {
+public:
+	static constexpr idx_t BUFFER_SIZE = STANDARD_VECTOR_SIZE;
+public:
+	WriteAvroGlobalState(ClientContext &context, FunctionData &bind_data_p, FileSystem &fs, const string &file_path);
+public:
+	//! Write a finished data block to the file
+	void WriteDataBlock(const_data_ptr_t data, idx_t size) {
+		lock_guard<mutex> flock(lock);
+		handle->Write((void *)data, size);
+	}
+
+	idx_t FileSize() {
+		lock_guard<mutex> flock(lock);
+		return handle->GetFileSize();
+	}
+public:
+	//! The in-memory stream to write to
+	MemoryStream stream;
+	FileSystem &fs;
+	//! The mutex for writing to the physical file
+	mutex lock;
+	//! The file handle to write to
+	unique_ptr<FileHandle> handle;
+
+	//! The writer for the file
+	avro_writer_t writer;
+	avro_file_writer_t file_writer;
+	//! The avro schema created from the input
+	avro_schema_t avro_schema;
 };
 
 } // namespace duckdb
